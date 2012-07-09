888257b33f7dd44bfab045fda98be7085921eb7a
David Dollar <ddollar@gmail.com>
2012-07-08 21:55:47 -0400

    cleanup with kept apps

--- lib/heroku/command/cleanup.rb
+++ lib/heroku/command/cleanup.rb
@@ -7,9 +7,17 @@ class Heroku::Command::Cleanup < Heroku::Command::Base
   # prompt to delete each app you own
   #
   def mine
-    heroku.list.each do |app, owner|
-      if owner == user && confirm("Delete #{app} (Y/N)?")
-        heroku.destroy(app)
+    kept = (settings["keep"] || [])
+    candidates = heroku.list.map do |app, owner|
+      next unless owner == user
+      next if kept.include?(app)
+      [ app, owner ]
+    end.compact
+    candidates.each_with_index do |(app, owner), idx|
+      print "[#{idx+1}/#{candidates.length}] Delete #{app}? [y/n/k] "
+      case $stdin.gets.chomp.strip.downcase
+        when "y" then heroku.destroy(app)
+        when "k" then keep(app)
       end
     end
   end
@@ -19,19 +27,50 @@ class Heroku::Command::Cleanup < Heroku::Command::Base
   # prompt to remove collaborator access to any non-heroku.com-owned app
   #
   def shared
-    heroku.list.each do |app, owner|
+    kept = (settings["keep"] || [])
+    candidates = heroku.list.map do |app, owner|
       next if owner == user
-      next if owner =~ /@heroku\.com$/
-      puts "Removing #{user} from app: #{app}"
-      heroku.remove_collaborator(app, user)
+      next if kept.include?(app)
+      [ app, owner ]
+    end.compact
+    candidates.each_with_index do |(app, owner), idx|
+      print "[#{idx+1}/#{candidates.length}] Remove #{user} from #{app}? [y/n/k] "
+      case $stdin.gets.chomp.strip.downcase
+        when "y" then heroku.remove_collaborator(app, user)
+        when "k" then keep(app)
+      end
     end
   end
 
-  private
+private
+
   def user
     Heroku::Command::Auth.new("").user
   rescue NoMethodError
     Heroku::Auth.user
+  end
+
+  def settings_file
+    File.expand_path("~/.heroku/cleanup.yml")
+  end
+
+  def settings
+    YAML.load_file(settings_file) rescue {}
+  end
+
+  def write_settings(settings)
+    File.open(settings_file, "w") do |file|
+      file.puts YAML.dump(settings)
+    end
+  end
+
+  def keep(app)
+    set = settings.dup
+    set["keep"] ||= []
+    set["keep"] << app
+    set["keep"].uniq!
+    set["keep"].sort!
+    write_settings set
   end
 
 end
